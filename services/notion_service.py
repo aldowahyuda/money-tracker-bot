912b0de76e4aaa47c41ffaf7c770686da1952c18
aldo <wahyudaaldo@gmail.com>
2026-01-18 02:58:27 +0700

    update services/notion_services.py

--- services/notion_service.py
+++ services/notion_service.py
@@ -1,38 +1,34 @@
-import os
 from datetime import datetime
+import os
 from notion_client import Client
 
 notion = Client(auth=os.getenv("NOTION_TOKEN"))
 DATABASE_ID = os.getenv("NOTION_DATABASE_ID")
 
 
-def insert_transaction(title: str, amount: int, tx_type: str):
-    return notion.pages.create(
-        parent={"database_id": DATABASE_ID},
-        properties={
-            "Transaction": {
-                "title": [{"text": {"content": title}}]
-            },
-            "Amount": {
-                "number": amount
-            },
-            "Type": {
-                "select": {"name": tx_type.capitalize()}
-            },
-            "Date": {
-                "date": {"start": datetime.now().isoformat()}
-            }
-        }
-    )
-
-
 def get_today_transactions():
     today = datetime.now().date().isoformat()
 
-    return notion.databases.query(
-        database_id=DATABASE_ID,
+    response = notion.search(
         filter={
-            "property": "Date",
-            "date": {"equals": today}
+            "property": "object",
+            "value": "page"
         }
-    )["results"]
+    )
+
+    results = []
+
+    for page in response["results"]:
+        props = page["properties"]
+
+        if "Date" not in props:
+            continue
+
+        date_prop = props["Date"].get("date")
+        if not date_prop:
+            continue
+
+        if date_prop["start"] == today:
+            results.append(page)
+
+    return results
